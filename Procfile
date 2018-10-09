18a59f16f9da3999f0d2a5597f4361beb5f28d3b
Jamker <Jamker@protonmail.com>
2018-10-09 18:15:18 +0300

    Переделал npm скрипты

--- Procfile
+++ Procfile
@@ -1,4 +1,5 @@
 client-dev: npm run client-dev
 client-production: npm run client-production
-server: npm run server
+server-production: npm run server-production
+server-dev: npm run server-dev
 react: npm run react
--- src/server.js
+++ src/server.js
@@ -360,5 +360,11 @@ app.post('/api/user', (req, res, next) => {
     });
   }
 });
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../dist/')));
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../dist/', 'index.html'));
+  });
+}
 app.listen(5000);
 
